7ce26ec25657930046963263bf2248d0c5dba321
Samwise-B <47277717+Samwise-B@users.noreply.github.com>
2023-07-26 17:42:08 +0100

    add cart-pole environment and imports

---
+++ main.py
@@ -0,0 +1,18 @@
+import gymnasium as gym
+import math
+import random
+import matplotlib
+import matplotlib.pyplot as plt
+from collections import namedtuple, deque
+from itertools import count
+
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torch.nn.functional as F
+
+env = gym.make("CartPole-v1")
+
+plt.ion()
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
